edc7ac447b8e6b004107628e7f7bf4f1a41fc95d
lizhihao <310645359@qq.com>
2021-04-18 22:17:09 +0800

    生成bit文件

--- 02_Shift/02_Shift.runs/impl_1/Board.tcl
+++ 02_Shift/02_Shift.runs/impl_1/Board.tcl
@@ -61,91 +61,23 @@ proc step_failed { step } {
 }
 
 
-start_step init_design
-set ACTIVE_STEP init_design
+start_step write_bitstream
+set ACTIVE_STEP write_bitstream
 set rc [catch {
-  create_msg_db init_design.pb
-  create_project -in_memory -part xc7a100tfgg484-2L
-  set_property design_mode GateLvl [current_fileset]
-  set_param project.singleFileAddWarning.threshold 0
+  create_msg_db write_bitstream.pb
+  open_checkpoint Board_routed.dcp
   set_property webtalk.parent_dir D:/vivado/02_Shift/02_Shift.cache/wt [current_project]
-  set_property parent.project_path D:/vivado/02_Shift/02_Shift.xpr [current_project]
-  set_property ip_output_repo D:/vivado/02_Shift/02_Shift.cache/ip [current_project]
-  set_property ip_cache_permissions {read write} [current_project]
-  add_files -quiet D:/vivado/02_Shift/02_Shift.runs/synth_1/Board.dcp
-  read_xdc D:/vivado/02_Shift/Board.xdc
-  link_design -top Board -part xc7a100tfgg484-2L
-  close_msg_db -file init_design.pb
+  catch { write_mem_info -force Board.mmi }
+  write_bitstream -force Board.bit 
+  catch {write_debug_probes -quiet -force Board}
+  catch {file copy -force Board.ltx debug_nets.ltx}
+  close_msg_db -file write_bitstream.pb
 } RESULT]
 if {$rc} {
-  step_failed init_design
+  step_failed write_bitstream
   return -code error $RESULT
 } else {
-  end_step init_design
-  unset ACTIVE_STEP 
-}
-
-start_step opt_design
-set ACTIVE_STEP opt_design
-set rc [catch {
-  create_msg_db opt_design.pb
-  opt_design 
-  write_checkpoint -force Board_opt.dcp
-  create_report "impl_1_opt_report_drc_0" "report_drc -file Board_drc_opted.rpt -pb Board_drc_opted.pb -rpx Board_drc_opted.rpx"
-  close_msg_db -file opt_design.pb
-} RESULT]
-if {$rc} {
-  step_failed opt_design
-  return -code error $RESULT
-} else {
-  end_step opt_design
-  unset ACTIVE_STEP 
-}
-
-start_step place_design
-set ACTIVE_STEP place_design
-set rc [catch {
-  create_msg_db place_design.pb
-  if { [llength [get_debug_cores -quiet] ] > 0 }  { 
-    implement_debug_core 
-  } 
-  place_design 
-  write_checkpoint -force Board_placed.dcp
-  create_report "impl_1_place_report_io_0" "report_io -file Board_io_placed.rpt"
-  create_report "impl_1_place_report_utilization_0" "report_utilization -file Board_utilization_placed.rpt -pb Board_utilization_placed.pb"
-  create_report "impl_1_place_report_control_sets_0" "report_control_sets -verbose -file Board_control_sets_placed.rpt"
-  close_msg_db -file place_design.pb
-} RESULT]
-if {$rc} {
-  step_failed place_design
-  return -code error $RESULT
-} else {
-  end_step place_design
-  unset ACTIVE_STEP 
-}
-
-start_step route_design
-set ACTIVE_STEP route_design
-set rc [catch {
-  create_msg_db route_design.pb
-  route_design 
-  write_checkpoint -force Board_routed.dcp
-  create_report "impl_1_route_report_drc_0" "report_drc -file Board_drc_routed.rpt -pb Board_drc_routed.pb -rpx Board_drc_routed.rpx"
-  create_report "impl_1_route_report_methodology_0" "report_methodology -file Board_methodology_drc_routed.rpt -pb Board_methodology_drc_routed.pb -rpx Board_methodology_drc_routed.rpx"
-  create_report "impl_1_route_report_power_0" "report_power -file Board_power_routed.rpt -pb Board_power_summary_routed.pb -rpx Board_power_routed.rpx"
-  create_report "impl_1_route_report_route_status_0" "report_route_status -file Board_route_status.rpt -pb Board_route_status.pb"
-  create_report "impl_1_route_report_timing_summary_0" "report_timing_summary -max_paths 10 -file Board_timing_summary_routed.rpt -pb Board_timing_summary_routed.pb -rpx Board_timing_summary_routed.rpx -warn_on_violation "
-  create_report "impl_1_route_report_incremental_reuse_0" "report_incremental_reuse -file Board_incremental_reuse_routed.rpt"
-  create_report "impl_1_route_report_clock_utilization_0" "report_clock_utilization -file Board_clock_utilization_routed.rpt"
-  create_report "impl_1_route_report_bus_skew_0" "report_bus_skew -warn_on_violation -file Board_bus_skew_routed.rpt -pb Board_bus_skew_routed.pb -rpx Board_bus_skew_routed.rpx"
-  close_msg_db -file route_design.pb
-} RESULT]
-if {$rc} {
-  write_checkpoint -force Board_routed_error.dcp
-  step_failed route_design
-  return -code error $RESULT
-} else {
-  end_step route_design
+  end_step write_bitstream
   unset ACTIVE_STEP 
 }
 
