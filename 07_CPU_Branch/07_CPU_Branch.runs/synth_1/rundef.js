d2bc01c0ed465c2a16102a5a84a7b7655286b6d0
Blueflame <310645359@qq.com>
2021-05-30 20:33:03 +0800

    板级

--- 07_CPU_Branch/07_CPU_Branch.runs/synth_1/rundef.js
+++ 07_CPU_Branch/07_CPU_Branch.runs/synth_1/rundef.js
@@ -23,7 +23,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 ISEStep( "vivado",
-         "-log CPU.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source CPU.tcl" );
+         "-log board_cpu.vds -m64 -product Vivado -mode batch -messageDb vivado.pb -notrace -source board_cpu.tcl" );
 
 
 
--- 07_CPU_Branch/board_cpu.v
+++ 07_CPU_Branch/board_cpu.v
@@ -6,15 +6,15 @@ module board_cpu(sw,
                  which,
                  seg,
                  enable);
-
+    
     input [1:32] sw;
     input [1:6] swb;
     output [1:32] led;
-    input clk; 
+    input clk;
     output [2:0] which;
     output [7:0] seg;
     output reg enable = 1;
-    wire [31:0] I,A,B,C,F;
+    wire [31:0] I,A,B,C,F,Shift_Out,PC;
     reg [32:0] data;
     reg [2:0]cnt = 0;
     wire [3:0] NZCV;
@@ -22,22 +22,22 @@ module board_cpu(sw,
     wire LA,LB,LC,LF;
     wire Write_PC,Write_IR,Write_Reg,S;
     wire rm_imm_s;
-    wire [1:0] rs_imm_s;
+    wire [1:0] rs_imm_s,PC_s;
     wire [3:0] ALU_OP;
     wire [2:0] SHIFT_OP;
-
+    wire rd_s,ALU_A_s,ALU_B_s;
     wire [1:0] test;
     wire [3:0] NZCV_New;
-
-    CPU cpu(swb[1],
-            swb[2],
-            I,
+    
+    CPU cpu(swb[1],       //时钟信号
+            swb[2],      //复位信号
+            I,         //指令机器码?
             A,
             B,
             C,
             F,
             NZCV,
-            Inst_addr,
+            Inst_addr, //PC取指令地址?
             Write_PC,
             Write_IR,
             Write_Reg,
@@ -49,40 +49,46 @@ module board_cpu(sw,
             rs_imm_s,
             ALU_OP,
             SHIFT_OP,
-            S);
-
+            S,
+            Shift_Out,
+            PC_s,
+            rd_s,
+            ALU_A_s,
+            ALU_B_s,
+            PC);
+    
     always @(posedge swb[6])
     begin
         case(cnt)
-        0:begin data <= {I,1'b1}; end
-        1:begin data <= {A,1'b1}; end
-        2:begin data <= {B,1'b1}; end
-        3:begin data <= {C,1'b1}; end
-        4:begin data <= {F,1'b1}; end
-        default:begin data <= {32'h88888888,1'b0}; end
+            0:begin data <= {I,1'b1}; end
+            1:begin data <= {A,1'b1}; end
+            2:begin data <= {B,1'b1}; end
+            3:begin data <= {C,1'b1}; end
+            4:begin data <= {F,1'b1}; end
+            5:begin data <= {Shift_Out,1'b1}; end
+            default:begin data <= {32'h88888888,1'b0}; end
         endcase
-        cnt <= (cnt+1)%6;
+        cnt <= (cnt+1)%7;
     end
-
-    assign led[29:32] = NZCV;
-    assign led[22:27] = Inst_addr;
-    assign led[20] = Write_PC;
-    assign led[19] = Write_IR;
-    assign led[18] = Write_Reg;
     
-    assign led[17] = LA;
-
-    assign led[16] = LB;
-    assign led[15] = LC;
-    assign led[14] = LF;
+    assign led[25:32] = PC[7:0];
+    assign led[23]    = Write_PC;
+    assign led[22]    = Write_IR;
+    assign led[21]    = Write_Reg;
+    assign led[20]    = S;
     
-    assign led[11] = rm_imm_s;
+    assign led[17:18] = PC_s;
+    
+    assign led[12:15] = NZCV;
+    
+    assign led[11]   = rm_imm_s;
     assign led[9:10] = rs_imm_s;
-    assign led[5:8] = ALU_OP;
-    assign led[2:4] = SHIFT_OP;
-    assign led[1] = S;
-
+    assign led[5:8]  = ALU_OP;
+    assign led[4]    = rd_s;
+    assign led[2]    = ALU_A_s;
+    assign led[1]    = ALU_B_s;
+    
     Display Display_Instance(.clk(clk), .data(data),
     .which(which), .seg(seg));
-
+    
 endmodule
